37e4639d5ab3a9f0494e31ff7878738d4dd61d8d
ajkristanto <a.j.kristanto@gmail.com>
2022-02-01 16:07:05 -0800

    Fixed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(2, SkillDemo.multiply(2, 2)); // should fail
+        assertEquals(2, SkillDemo.multiply(2, 1)); // should fail
     }
 }
